fe9a719e58bf76715089c3f51224024c2b42400a
Mark Kwon <redhero8830@gmail.com>
2024-06-20 17:05:24 +0900

    style: 추가

---
+++ src/pages/admin/mileage/mileage.js
@@ -0,0 +1,47 @@
+function app() {
+  toggleSwitch();
+  fetchMileageData();
+  makeModal();
+}
+
+function toggleSwitch() {
+  const toggleSwitch = document.querySelector('#toggleSwitch');
+  const toggleText = document.querySelector('#toggleText');
+
+  toggleSwitch.addEventListener('click', () =>
+    // this.checked
+    toggleSwitch.checked
+      ? (toggleText.textContent = '확인')
+      : (toggleText.textContent = '미확인')
+  );
+}
+
+async function fetchMileageData() {
+  const response = await fetch('../mileage/mileage.json');
+  const data = await response.json();
+  fillMileageList(data);
+}
+function fillMileageList(data) {
+  const mileageList = document.querySelector('.mileage-list');
+
+  data.forEach((item) => {
+    const div = document.createElement('div');
+    div.className = 'mileage-list__item';
+    div.style.backgroundImage = `url(${item.image})`;
+    div.innerHTML = `<div class="mileage-list__title">
+      <h3>${item.category}</h3>
+      <h5>${item.data}</h5>
+      </div>`;
+
+    makeModal(div);
+
+    mileageList.appendChild(div);
+  });
+}
+
+function makeModal(div) {
+  div.classList.add('modalOpenBtn');
+}
+
+//모든 함수 포함
+document.addEventListener('DOMContentLoaded', app);
